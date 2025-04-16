36ea0f3f40f93a7340943717d9c9878636186ea1
WolfGamer2 <rushilchopra@gmail.com>
2025-04-16 12:28:50 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'; // Make sure Tailwind styles are loaded
 
 const rootElement = document.getElementById('app');
 const root = ReactDOM.createRoot(rootElement);
