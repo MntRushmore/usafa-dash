46051ceecb202b80ed367dee81a6187fd9897279
WolfGamer2 <rushilchopra@gmail.com>
2025-04-16 12:26:45 -0700

    push

--- main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';  // Import your App.jsx file
-
-const rootElement = document.getElementById('app');  // The div in index.html
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(<App />);  // Render the App component into the DOM
